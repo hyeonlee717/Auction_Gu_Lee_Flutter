dba2fec8b4a4a71fe5dbdfb023e1b9f358612bb7
Hyeon Lee <103764100+hyeonlee717@users.noreply.github.com>
2024-11-09 14:36:08 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:auction_gu_lee_flutter/screen/lobbyscreen.dart'; // 경로를 프로젝트 이름에 맞게 수정하세요.
+import 'package:auction_gu_lee_flutter/screen/lobby_screen.dart'; // 경로를 프로젝트 이름에 맞게 수정하세요.
 
 void main() {
   runApp(const MyApp());
---
+++ lib/screen/past_auction_list_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class PastAuctionListScreen extends StatelessWidget {
+  const PastAuctionListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.swap_vert,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_none,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.search,
+                  size: 30,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.blueAccent,
+        title: const Text(
+          '지난 경매',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 6.0),
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.all(15.0),
+            margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+            decoration: BoxDecoration(
+              color: const Color(0xFFF4EDDB),
+              borderRadius: BorderRadius.circular(15.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: const Offset(3, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 80,
+                  height: 80,
+                  color: Colors.blue,
+                ), // 경매 아이콘
+                const SizedBox(width: 30),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '경매 항목 $index',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ), // 경매 제목
+                      const SizedBox(height: 10),
+                      const Text(
+                        '경매 종료',
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ), // 경매 설명
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '시작가격 $index',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        '최고가격 $index',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 10),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
